1b6081b364e0464e45c107eb1c6ec89a7d704534
Aaron Jones <aaronjjj95@aol.com>
2024-10-02 21:50:03 +0000

    added product details function

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
 def all_products(request):
@@ -11,3 +11,13 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request):
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
