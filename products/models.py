80959c188ddf8ff5016e357925c9789b782d7a1f
Aaron Jones <aaronjjj95@aol.com>
2024-10-04 20:42:52 +0000

    sorting section has been made for products

--- products/models.py
+++ products/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Category (models.Model):
-    name= models.CharField(max_length=254)
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
