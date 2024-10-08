06c6afe63564f6434d6b1d4d95ef97be144ad266
Aaron Jones <aaronjjj95@aol.com>
2024-10-08 19:00:42 +0000

    added subtotal and quantity to basket

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from products.models import Product
 
 # Create your views here.
 def view_basket(request):
@@ -17,3 +18,28 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+def adjust_basket(request, item_id):
+    
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop[item_id]
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+def remove_from_basket(request, item_id):
+    try:
+        basket = request.session.get('basket', {})
+        
+        item_id.delete()
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
+
