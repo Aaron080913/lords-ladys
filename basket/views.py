d725892b60ef4c8144fc006d525709e7be72adb8
Aaron Jones <aaronjjj95@aol.com>
2024-10-08 19:41:05 +0000

    added messages to site

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 from products.models import Product
 
 # Create your views here.
@@ -6,7 +7,8 @@ def view_basket(request):
     return render(request, 'basket/basket.html')
 
 def add_to_basket(request, item_id):
-    
+
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -15,6 +17,7 @@ def add_to_basket(request, item_id):
         basket[item_id] += quantity
     else:
         basket[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
