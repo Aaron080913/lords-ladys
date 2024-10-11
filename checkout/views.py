e0693f4a5cdb040431f318427ddd0ac7a1d8a04f
Aaron Jones <aaronjjj95@aol.com>
2024-10-11 14:37:15 +0000

    added checkout views and templates

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import OrderForm
 
 # Create your views here.
+def checkout(request):
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "No Items found in your basket, please add items to proceed to checkout.")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
