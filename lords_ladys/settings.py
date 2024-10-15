952a4546bd94ab2e1a423cb00073ff57c94ddb38
Aaron Jones <aaronjjj95@aol.com>
2024-10-15 19:56:33 +0000

    heroku deployment

--- lords_ladys/settings.py
+++ lords_ladys/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'mf+lv6h*ht@dr5-gzbcg%=$3o$$ojlt1rx6=q869*-1@vobke_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -121,12 +121,13 @@ if 'DATABASE_URL' in os.environ:
     DATABASE = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 #DATABASES = {
 #    'default': dj_database_url.parse('postgres://un8qb19pztk:Gd1yoZXuK4MI@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/tart_snack_react_310425')
